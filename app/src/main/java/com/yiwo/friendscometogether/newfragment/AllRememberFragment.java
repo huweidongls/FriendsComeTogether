02502e6294bd3d90b6a953dbb138a585be63f8ba
huweidong <466463054@qq.com>
2018-12-20 12:07:29 +0800

    Revert "123123"
    This reverts commit 04858f4d658a904b2dd247ec96ae59b13e1eb731.

--- app/src/main/java/com/yiwo/friendscometogether/newfragment/AllRememberFragment.java
+++ app/src/main/java/com/yiwo/friendscometogether/newfragment/AllRememberFragment.java
@@ -169,10 +169,12 @@ public class AllRememberFragment extends BaseFragment {
                                         toDialog(getContext(), "提示", "是否取消收藏", new DialogInterface.OnClickListener() {
                                             @Override
                                             public void onClick(final DialogInterface dialog, int which) {
-                                                String token = getToken(NetConfig.BaseUrl + NetConfig.deleteCollectionUrl);
-                                                ViseHttp.POST(NetConfig.deleteCollectionUrl)
+                                                String token = getToken(NetConfig.BaseUrl + NetConfig.articleCollectionUrl);
+                                                ViseHttp.POST(NetConfig.articleCollectionUrl)
                                                         .addParam("app_key", token)
                                                         .addParam("id", mList1.get(i).getFID())
+                                                        .addParam("uid", uid)
+                                                        .addParam("type", "1")
                                                         .request(new ACallback<String>() {
                                                             @Override
                                                             public void onSuccess(String data) {
