0750a4aa03f52a721c759852304cad25bfdb0050
huweidong <466463054@qq.com>
2019-01-07 14:45:07 +0800

    123123

---
+++ app/src/main/java/com/yiwo/friendscometogether/newpage/SuperLikeSxActivity.java
@@ -0,0 +1,15 @@
+package com.yiwo.friendscometogether.newpage;
+
+import android.os.Bundle;
+
+import com.yiwo.friendscometogether.R;
+import com.yiwo.friendscometogether.base.BaseActivity;
+
+public class SuperLikeSxActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_like_sx);
+    }
+}
