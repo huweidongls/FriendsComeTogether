9b2e3f1a8587f156b41e54e060a856f5a81f03fa
liujichang <1391407358@qq.com>
2019-01-07 14:40:59 +0800

    111

---
+++ app/src/main/java/com/yiwo/friendscometogether/newpage/SuperLikeActivity.java
@@ -0,0 +1,15 @@
+package com.yiwo.friendscometogether.newpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yiwo.friendscometogether.R;
+
+public class SuperLikeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_like);
+    }
+}
