c2a94fc6ac4702192b09a4fdd6f370f0948029dc
huweidong <466463054@qq.com>
2019-01-08 16:16:33 +0800

    123123

---
+++ app/src/main/java/com/yiwo/friendscometogether/newadapter/EditorLabelListAdapter.java
@@ -0,0 +1,53 @@
+package com.yiwo.friendscometogether.newadapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yatoooon.screenadaptation.ScreenAdapterTools;
+import com.yiwo.friendscometogether.R;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2019/1/8.
+ */
+
+public class EditorLabelListAdapter extends RecyclerView.Adapter<EditorLabelListAdapter.ViewHolder> {
+
+    private Context context;
+    private List<String> data;
+
+    public EditorLabelListAdapter(List<String> data) {
+        this.data = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        this.context = parent.getContext();
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_editor_label_list, parent, false);
+        ScreenAdapterTools.getInstance().loadView(view);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+}
--- app/src/main/java/com/yiwo/friendscometogether/newpage/EditorLabelListActivity.java
+++ app/src/main/java/com/yiwo/friendscometogether/newpage/EditorLabelListActivity.java
@@ -1,16 +1,32 @@
 package com.yiwo.friendscometogether.newpage;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.yatoooon.screenadaptation.ScreenAdapterTools;
 import com.yiwo.friendscometogether.R;
 import com.yiwo.friendscometogether.base.BaseActivity;
 import com.yiwo.friendscometogether.imagepreview.StatusBarUtils;
+import com.yiwo.friendscometogether.newadapter.EditorLabelListAdapter;
 
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class EditorLabelListActivity extends BaseActivity {
+
+    private Context context = EditorLabelListActivity.this;
+
+    @BindView(R.id.rv)
+    RecyclerView recyclerView;
+
+    private EditorLabelListAdapter adapter;
+    private List<String> mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +37,23 @@ public class EditorLabelListActivity extends BaseActivity {
         ScreenAdapterTools.getInstance().loadView(getWindow().getDecorView());
         ButterKnife.bind(EditorLabelListActivity.this);
 
+        initData();
+
     }
+
+    private void initData() {
+
+
+
+    }
+
+    @OnClick({R.id.rl_back})
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.rl_back:
+                finish();
+                break;
+        }
+    }
+
 }
